416b256070f6d7a97ad38b24387a373355d58aa1
Hannah Davydova <hannah.davydova@gmail.com>
2020-06-12 17:35:30 +0100

    Install mongoose and connect to local mongodb

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// local database
+mongoose.connect("mongodb://localhost:27017/toDoDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("MongoDB database connection established.");
+});
+
+// listen
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
