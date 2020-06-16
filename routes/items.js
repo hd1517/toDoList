9181a4f0b9098995f4a6cb6f6b50a3a6549a6886
Hannah Davydova <hannah.davydova@gmail.com>
2020-06-16 12:18:54 +0100

    Separate items route

---
+++ routes/items.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const router = require("express").Router();
+let List = require("../models/listModel");
+
+// ADD ITEM TO LIST
+router.route("/addItem").post((req, res) => {
+    const newItem = req.body.newItem;
+    const forList = req.body.list;
+
+    List.findOne({ name: forList }, function (err, addToList) {
+        const item = {
+            content: newItem,
+        }
+        addToList.toDo.push(item);
+        addToList.save();
+    });
+
+res.redirect("/");
+});
+
+
+module.exports = router;
