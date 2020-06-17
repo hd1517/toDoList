4959f22ea662b553b68a64e8223d4b7f53b84f51
Hannah Davydova <hannah.davydova@gmail.com>
2020-06-17 19:47:41 +0100

    Add close icon for items on hover

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,36 +1,41 @@
 $(document).ready(function () {
+  $("#dismiss, .overlay").on("click", function () {
+    // hide sidebar
+    $("#sidebar").removeClass("active");
+    // hide overlay
+    $(".overlay").removeClass("active");
+  });
 
-    $('#dismiss, .overlay').on('click', function () {
-        // hide sidebar
-        $('#sidebar').removeClass('active');
-        // hide overlay
-        $('.overlay').removeClass('active');
-    });
+  $("#sidebarBtn").on("click", function () {
+    // open sidebar
+    $("#sidebar").addClass("active");
+    // fade in the overlay
+    $(".overlay").addClass("active");
+    $(".collapse.in").toggleClass("in");
+    $("a[aria-expanded=true]").attr("aria-expanded", "false");
+  });
 
-    $('#sidebarBtn').on('click', function () {
-        // open sidebar
-        $('#sidebar').addClass('active');
-        // fade in the overlay
-        $('.overlay').addClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
+  $("#addNewListLink").on("click", function () {
+    // hide sidebar
+    $("#sidebar").removeClass("active");
+    // hide overlay
+    $(".overlay").removeClass("active");
+  });
 
-    $('#addNewListLink').on('click', function() {
-        // hide sidebar
-        $('#sidebar').removeClass('active');
-        // hide overlay
-        $('.overlay').removeClass('active');
-    });
+  if ($(".errorMsg").text() != "") {
+    $("#errorModal").modal("show");
+  }
 
-    if ($('.errorMsg').text() != "") {
-        $('#errorModal').modal("show");
+  if (!$(".doneItems")[0]) {
+    $(".doneBox").hide();
+  }
+
+  $(".item").hover(
+    function () {
+      $(this).find(".close").show();
+    },
+    function () {
+      $(this).find(".close").hide();
     }
-
-    if (!$(".doneItems")[0]){
-        $(".doneBox").hide();
-    } 
-
-    
-
+  );
 });
