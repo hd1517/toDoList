a2294bd1f2833ee9a2e51f1285ebfd5befe701f0
Hannah Davydova <hannah.davydova@gmail.com>
2020-06-19 15:25:53 +0100

    Changed .save() to .create() to save new Today and Work lists if all lists are deleted

--- routes/lists.js
+++ routes/lists.js
@@ -1,143 +1,142 @@
 const mongoose = require("mongoose");
 const router = require("express").Router();
-const {Item} = require("../models/itemModel");
+const { Item } = require("../models/itemModel");
 const List = require("../models/listModel");
 const _ = require("lodash");
 
 let errorMsg = "";
 let urlParam = "";
+const currentYear = new Date().getFullYear();
 
 const item1 = new Item({
-    content: "Eat"
+  content: "Eat",
 });
 
 const item2 = new Item({
-    content: "Sleep"
+  content: "Sleep",
 });
 
 const item3 = new Item({
-    content: "Clean"
+  content: "Clean",
 });
 
 const item4 = new Item({
-    content: "Read"
+  content: "Read",
 });
 
-const list1 = new List({
-    name: "Today",
-    formattedName: "today",
-    toDo: [item1, item2],
-    done: [item3, item4]
-});
+const list1 = {
+  name: "Today",
+  formattedName: "today",
+  toDo: [item1, item2],
+  done: [item3, item4],
+};
 
-const list2 = new List({
-    name: "Work",
-    formattedName: "work",
-    toDo: [item4],
-    done: []
-});
-
-const currentYear = new Date().getFullYear();
+const list2 = {
+  name: "Work",
+  formattedName: "work",
+  toDo: [item4],
+  done: [],
+};
 
 // Home page
-router.route("/").get((req, res) => {
-    List.find({}, function (err, lists) {
-        if (lists.length === 0) {
-            list1.save();
-            list2.save();
-            res.redirect("/");
-        } else {
-            List.findOne({}, function (err, foundList) {
-                res.render("index", {
-                    currentYear: currentYear,
-                    listTitles: lists,
-                    showList: foundList,
-                    errorMsg: errorMsg,
-                    urlParam: urlParam
-                });
-            });
-        }
+router.route("/").get(async (req, res) => {
+  List.find().then(lists => {
+      // If no lists, create Today and Work list
+      if (lists.length === 0) {
+        List.create(list1);
+        List.create(list2);
+        res.redirect("/");
+      } else {
+        res.render("index", {
+            currentYear: currentYear,
+            listTitles: lists,
+            showList: lists[0],
+            errorMsg: errorMsg,
+            urlParam: urlParam,
+          }); 
+      }
+  }).catch(err => console.log(err));
 
-    });
 });
 
 // Show list using parameters
 router.route("/:listName").get((req, res) => {
-    const urlTitle = req.params.listName;
+  const urlTitle = req.params.listName;
 
-    List.find({}, (err, listTitles) => {
-        List.findOne({formattedName: urlTitle}, (err, thisList) => {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("index", {
-                    currentYear: currentYear,
-                    listTitles: listTitles,
-                    showList: thisList,
-                    errorMsg: errorMsg,
-                    urlParam: urlParam
-                });
-            }
+  // Find list titles for sidebar
+  List.find({}, (err, listTitles) => {
+      // Find requested list
+    List.findOne({ formattedName: urlTitle }, (err, thisList) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("index", {
+          currentYear: currentYear,
+          listTitles: listTitles,
+          showList: thisList,
+          errorMsg: errorMsg,
+          urlParam: urlParam,
         });
+      }
     });
+  });
 });
 
 // Add New List
 router.route("/addNewList").post((req, res) => {
-    let newTitle = _.capitalize(req.body.newListTitle);
-        lowerCaseTitle = _.lowerCase(newTitle);
-        formattedTitle = lowerCaseTitle.replace(/[^a-zA-Z0-9]/g, '_');
-    
-    List.findOne({ formattedName: formattedTitle }, function (err, listExists) {
-        if (!listExists) {
-            const list = new List({
-                name: newTitle,
-                formattedName: formattedTitle
-            });
+  let newTitle = _.capitalize(req.body.newListTitle);
+  lowerCaseTitle = _.lowerCase(newTitle);
+  formattedTitle = lowerCaseTitle.replace(/[^a-zA-Z0-9]/g, "_");
 
-            list.save();
-            res.redirect("/" + formattedTitle);
-        } else {
-            errorMsg = "A list with this title already exists, you have now been redirected.";
-            urlParam = formattedTitle;
-            res.redirect("/" + formattedTitle);
-        }
-    });
+  List.findOne({ formattedName: formattedTitle }, function (err, listExists) {
+    if (!listExists) {
+      const list = new List({
+        name: newTitle,
+        formattedName: formattedTitle,
+      });
+
+      list.save();
+      res.redirect("/" + formattedTitle);
+    } else {
+      errorMsg =
+        "A list with this title already exists, you have now been redirected.";
+      urlParam = formattedTitle;
+      res.redirect("/" + formattedTitle);
+    }
+  });
 });
 
 // If user tries to add new list title that already exists, redirect
 router.route("/redirect").post((req, res) => {
-    if (errorMsg != "") {
-        errorMsg = "";
-        res.redirect("/" + urlParam);
-    }
+  if (errorMsg != "") {
+    errorMsg = "";
+    res.redirect("/" + urlParam);
+  }
 });
 
 // Delete list
 router.route("/deleteList").post((req, res) => {
-    const listToDelete = req.body.listToDelete;
+  const listToDelete = req.body.listToDelete;
 
-    List.findByIdAndRemove(listToDelete, function(err) {
-        if (!err) {
-            res.redirect("/");
-        }
-    });
-})
+  List.findByIdAndDelete(listToDelete)
+    .then(() => res.redirect("/"))
+    .catch((err) => res.status(400).json("Error: " + err));
+
+});
 
 // Update list title
 router.route("/editTitle").post((req, res) => {
-    let newTitle = req.body.newListTitle;
-    let newFormattedName = _.lowerCase(newTitle).replace(/[^a-zA-Z0-9]/g, '_');
-    const oldFormattedName = req.body.formattedTitle;
+  let newTitle = _.capitalize(req.body.newListTitle);
+  let newFormattedName = _.lowerCase(newTitle).replace(/[^a-zA-Z0-9]/g, "_");
+  const oldFormattedName = req.body.formattedTitle;
 
-    List.findOneAndUpdate(
-        { formattedName: oldFormattedName},
-        { name : newTitle, formattedName: newFormattedName },
-        function (err) {
-          res.redirect("/" + newFormattedName);
-        }
-      ); 
-})
-
+  List.findOneAndUpdate(
+    { formattedName: oldFormattedName },
+    { name: newTitle, formattedName: newFormattedName },
+    function (err) {
+      res.redirect("/" + newFormattedName);
+    }
+  );
+});
 
 module.exports = router;
